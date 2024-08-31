315643d0b05823b92b07724d74bafbd1a4f7a28d
yuuboukun <pikahikari87@gmail.com>
2024-08-31 09:29:54 +0900

    240825_yuuzou.yamadaのブランチのコミット012

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,4 +1,6 @@
-var swiper = new Swiper('.swiper', {
+// カルーセルの初期化
+const swiper = new Swiper('.swiper', {
+  grabCursor: true,
   slidesPerView: 1,  // 表示するスライド数
   spaceBetween: 15,  // スライド間のスペース
   loop: true,        // ループ設定
@@ -6,4 +8,6 @@ var swiper = new Swiper('.swiper', {
     el: '.swiper-pagination',
     clickable: true,
   },
+  initialSlide: 1,   // 最初に表示するスライドを2番目に設定
 });
+
