f0b14f2cf4394f62a609a48ab0f376916d201481
yuuboukun <pikahikari87@gmail.com>
2024-09-04 08:56:30 +0900

    240825_yuuzou.yamadaのブランチのコミット018

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,13 +1,13 @@
 // カルーセル
 const swiper = new Swiper('.swiper', {
-  grabCursor: true,
-  slidesPerView: 1,
-  spaceBetween: 15,
-  loop: true,
-  pagination: {
+    grabCursor: true,
+    slidesPerView: 1,
+    spaceBetween: 15,
+    loop: true,
+    pagination: {
     el: '.swiper-pagination',
     clickable: true,
-  },
-  initialSlide: 1,
+    },
+    initialSlide: 1,
 });
 
